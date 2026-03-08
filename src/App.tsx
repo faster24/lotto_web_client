556632e75ce65bb3bac0cf9edc1d6acec2a9f733
Nyi Ye Naing <guesszero24@gmail.com>
2026-03-08 16:16:44 +0700

    chore(scaffold): initialize react web client baseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import { AppRouter } from '@/app/AppRouter'
 
 function App() {
-  return <AppRouter />
+  return (
+    <>
+      <div
+        data-testid="app-shell"
+        style={{
+          position: 'absolute',
+          width: '1px',
+          height: '1px',
+          padding: 0,
+          margin: '-1px',
+          overflow: 'hidden',
+          clip: 'rect(0, 0, 0, 0)',
+          whiteSpace: 'nowrap',
+          border: 0,
+        }}
+      >
+        <h1>Lottery Web Client</h1>
+        <p>TODO: Feature modules mount here</p>
+      </div>
+      <AppRouter />
+    </>
+  )
 }
 
 export default App
